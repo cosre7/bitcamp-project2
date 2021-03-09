f0dc2a7d63a5e0cb4e0a97278da0240136b7c60b
cosre7 <cosre7@gmail.com>
2021-03-09 14:01:56 +0900

    29-d

--- bitcamp-project-server/app/src/main/java/com/eomcs/pms/ServerApp.java
+++ bitcamp-project-server/app/src/main/java/com/eomcs/pms/ServerApp.java
@@ -1,8 +1,35 @@
 package com.eomcs.pms;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 // 데이터를 파일에 보관하고 꺼내는 일을 할 애플리케이션
 public class ServerApp {
   public static void main(String[] args) {
-    System.out.println("파일 관리 서버");
+    // 클라이언트 연결을 기다리는 서버 소켓 생성
+    try (ServerSocket serverSocket = new ServerSocket(8888)) {
+
+      // 클라이언트와 연결 수행
+      try (Socket socket = serverSocket.accept();
+          DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+          DataInputStream in = new DataInputStream(socket.getInputStream())) {
+
+        String response = in.readUTF();
+        System.out.println(response);
+
+        out.writeUTF("success");
+        out.flush();
+
+      } catch (Exception e) {
+        System.out.println("클라이언트의 요청을 처리하는 중에 오류 발생!");
+        e.printStackTrace();
+      }
+
+    } catch (Exception e) {
+      System.out.println("서버 실행 중 오류 발생!");
+      e.printStackTrace();
+    }
   }
 }
