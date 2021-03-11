adb9554464203d05918ff42a9275443109c685f7
cosre7 <cosre7@gmail.com>
2021-03-11 17:38:37 +0900

    29-f

--- bitcamp-project-client/app/src/main/java/com/eomcs/pms/ClientApp.java
+++ bitcamp-project-client/app/src/main/java/com/eomcs/pms/ClientApp.java
@@ -6,7 +6,11 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import com.eomcs.driver.Statement;
 import com.eomcs.pms.handler.BoardAddHandler;
+import com.eomcs.pms.handler.BoardDeleteHandler;
+import com.eomcs.pms.handler.BoardDetailHandler;
 import com.eomcs.pms.handler.BoardListHandler;
+import com.eomcs.pms.handler.BoardSearchHandler;
+import com.eomcs.pms.handler.BoardUpdateHandler;
 import com.eomcs.pms.handler.Command;
 import com.eomcs.util.Prompt;
 
@@ -36,10 +40,10 @@ public class ClientApp {
 
     commandMap.put("/board/add", new BoardAddHandler());
     commandMap.put("/board/list", new BoardListHandler());
-    //    commandMap.put("/board/detail", new BoardDetailHandler());
-    //    commandMap.put("/board/update", new BoardUpdateHandler());
-    //    commandMap.put("/board/delete", new BoardDeleteHandler());
-    //    commandMap.put("/board/search", new BoardDeleteHandler());
+    commandMap.put("/board/detail", new BoardDetailHandler());
+    commandMap.put("/board/update", new BoardUpdateHandler());
+    commandMap.put("/board/delete", new BoardDeleteHandler());
+    commandMap.put("/board/search", new BoardSearchHandler());
     //
     //    commandMap.put("/member/add", new MemberAddHandler());
     //    commandMap.put("/member/list", new MemberListHandler());
--- bitcamp-project-client/app/src/main/java/com/eomcs/pms/handler/BoardDeleteHandler.java
+++ bitcamp-project-client/app/src/main/java/com/eomcs/pms/handler/BoardDeleteHandler.java
@@ -1,35 +1,18 @@
 package com.eomcs.pms.handler;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
+import com.eomcs.driver.Statement;
 import com.eomcs.util.Prompt;
 
 public class BoardDeleteHandler implements Command {
 
   @Override
-  public void service(DataInputStream in, DataOutputStream out) throws Exception {
+  public void service(Statement stmt) throws Exception {
     System.out.println("[게시글 삭제]");
 
     int no = Prompt.inputInt("번호? ");
 
-    // 서버에 해당 번호의 게시글이 있는지 조회한다.
-    out.writeUTF("board/select");
-    out.writeInt(1);
-    out.writeUTF(Integer.toString(no));
-    out.flush();
+    stmt.executeQuery("board/select", Integer.toString(no));
 
-    // 서버의 응답을 읽는다.
-    String status = in.readUTF();
-    in.readInt();
-    // 서버에서 보낸 게시글을 사용할 일이 없기 때문에 리턴 값을 무시한다.
-    // - 중요한 것은 서버가 출력했으면 클라이언트는 필요가 없더라도 읽어야 한다.
-    // - 그것이 통신 프로토콜이다.
-    String data = in.readUTF();
-
-    if (status.equals("error")) {
-      System.out.println(data);
-      return;
-    }
 
     String input = Prompt.inputString("정말 삭제하시겠습니까?(y/N) ");
 
@@ -37,20 +20,9 @@ public class BoardDeleteHandler implements Command {
       System.out.println("게시글 삭제를 취소하였습니다.");
       return;
     }
-    // 서버에 데이터 삭제를 요청한다.
-    out.writeUTF("board/delete");
-    out.writeInt(1);
-    out.writeUTF(Integer.toString(no));
-    out.flush();
 
-    // 서버의 응답을 읽는다.
-    status = in.readUTF();
-    in.readInt();
+    stmt.executeUpdate("board/delete", Integer.toString(no));
 
-    if (status.equals("error")) {
-      System.out.println(in.readUTF());
-      return;
-    }
     System.out.println("게시글을 삭제하였습니다.");
   }
 }
