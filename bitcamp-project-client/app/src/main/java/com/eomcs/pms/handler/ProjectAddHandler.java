fe770fb8a469d494892b5865dabff359ba86912e
cosre7 <cosre7@gmail.com>
2021-03-26 19:36:24 +0900

    project

--- bitcamp-project-client/app/src/main/java/com/eomcs/pms/handler/ProjectAddHandler.java
+++ bitcamp-project-client/app/src/main/java/com/eomcs/pms/handler/ProjectAddHandler.java
@@ -44,6 +44,12 @@ public class ProjectAddHandler implements Command {
         PreparedStatement stmt2 = con.prepareStatement(
             "insert into pms_member_project(member_no,project_no) values(?,?)")) {
 
+      // 수동 커밋으로 설정한다.
+      // - pms_project 테이블과 pms_member_project 테이블에 모두 성공적으로 데이터를 저장했을 때
+      //   작업을 완료한다.
+      // 
+      con.setAutoCommit(false); // 의미 => 트랜잭션 시작 
+
       // 1) 프로젝트를 추가한다.
       stmt.setString(1, p.getTitle());
       stmt.setString(2, p.getContent());
@@ -60,9 +66,14 @@ public class ProjectAddHandler implements Command {
 
       // 2) 프로젝트에 팀원들을 추가한다.
       for (Member member : p.getMembers()) {
-        stmt2.setInt(member.getNo(), p.getNo());
+        stmt2.setInt(1, member.getNo());
+        stmt2.setInt(2, p.getNo());
         stmt2.executeUpdate();
       }
+
+      // 프로젝트 정보 뿐만 아니라 팀원 정보도 정상적으로 입력되었다면,
+      // 실제 테이블에 데이터를 적용한다.
+      con.commit(); // 의미 : 트랜잭션 종료
 
       System.out.println("프로젝트를 등록하였습니다.");
     }
