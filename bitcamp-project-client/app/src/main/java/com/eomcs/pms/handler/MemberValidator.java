f9106714d8b16164aebe5acb68e8ef674f215a73
cosre7 <cosre7@gmail.com>
2021-03-26 10:05:48 +0900

    .

--- bitcamp-project-client/app/src/main/java/com/eomcs/pms/handler/MemberValidator.java
+++ bitcamp-project-client/app/src/main/java/com/eomcs/pms/handler/MemberValidator.java
@@ -4,25 +4,35 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import com.eomcs.pms.domain.Member;
+import com.eomcs.util.Prompt;
 
 public class MemberValidator {
 
   public String inputMember(String promptTitle) throws Exception {
 
-    Member member = new Member();
+    while (true) {
+      String name = Prompt.inputString(promptTitle);
+      if (name.length() == 0) {
+        return null;
+      } 
 
-    try (Connection con = DriverManager.getConnection( //
-        "jdbc:mysql://localhost:3306/studydb?user=study&password=1111");
-        PreparedStatement stmt = con.prepareStatement( //
-            "select * from pms_member where name=?")) {
+      try (Connection con = DriverManager.getConnection( //
+          "jdbc:mysql://localhost:3306/studydb?user=study&password=1111");
+          PreparedStatement stmt = con.prepareStatement( //
+              "select count(*) from pms_member where name=?")) {
+        // name을 가진 데이터가 pms_member에 몇개가 있냐?
+        // 없으면 0 -> 무조건 결과는 있다!
 
-      stmt.setString(1, member.getName());
+        stmt.setString(1, name);
 
-      try (ResultSet rs = stmt.executeQuery()) {
-        if (!rs.next())
-          return null;
-        return member.getName();
+        try (ResultSet rs = stmt.executeQuery()) {
+          rs.next(); // 서버의 결과를 가져온다.
+          if (rs.getInt(1) > 0) {
+            // 1 = count(*) // 어차피 컬럼은 한개!
+            return name;
+          } 
+          System.out.println("등록되지 않은 회원입니다.");
+        }
       }
     }
   }
