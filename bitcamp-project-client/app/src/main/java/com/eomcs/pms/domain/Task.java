cd88b84ca45f1d848fd358bef432792706baa841
cosre7 <cosre7@gmail.com>
2021-03-29 12:05:45 +0900

    .

--- bitcamp-project-client/app/src/main/java/com/eomcs/pms/domain/Task.java
+++ bitcamp-project-client/app/src/main/java/com/eomcs/pms/domain/Task.java
@@ -6,16 +6,19 @@ public class Task {
   private int no;
   private String content;
   private Date deadline;
-  private Member owner;
+  private Member owner; // 1. 멤버객체 주소 저장방법
   private int status;
-
-  public Task() {}
+  private int projectNo; // 2. 실제 projectNo를 사용
+  private String projectTitle; // - 추가적으로 projectTitle을 사용
 
   @Override
   public String toString() {
     return "Task [no=" + no + ", content=" + content + ", deadline=" + deadline + ", owner=" + owner
-        + ", status=" + status + "]";
+        + ", status=" + status + ", projectNo=" + projectNo + ", projectTitle=" + projectTitle
+        + "]";
   }
+
+  public Task() {}
 
   public int getNo() {
     return no;
@@ -49,6 +52,22 @@ public class Task {
   }
   public void setStatus(int status) {
     this.status = status;
+  }
+
+  public int getProjectNo() {
+    return projectNo;
+  }
+
+  public void setProjectNo(int projectNo) {
+    this.projectNo = projectNo;
+  }
+
+  public String getProjectTitle() {
+    return projectTitle;
+  }
+
+  public void setProjectTitle(String projectTitle) {
+    this.projectTitle = projectTitle;
   }
 
   public static Object getStatusLabel(int status) {
--- bitcamp-project-client/app/src/main/java/com/eomcs/pms/handler/TaskAddHandler.java
+++ bitcamp-project-client/app/src/main/java/com/eomcs/pms/handler/TaskAddHandler.java
@@ -3,6 +3,10 @@ package com.eomcs.pms.handler;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import com.eomcs.pms.domain.Project;
 import com.eomcs.pms.domain.Task;
 import com.eomcs.util.Prompt;
 
@@ -18,6 +22,47 @@ public class TaskAddHandler implements Command {
   public void service() throws Exception {
     System.out.println("[작업 등록]");
 
+    // 1) 현재 등록된 프로젝트 목록을 가져온다.
+    List<Project> projects = new ArrayList<>();
+    try (Connection con = DriverManager.getConnection(
+        "jdbc:mysql://localhost:3306/studydb?user=study&password=1111");
+        PreparedStatement stmt = con.prepareStatement(
+            "select no, title, from pms_project p order by title asc");
+        ResultSet rs = stmt.executeQuery()) {
+
+
+      while (rs.next()) {
+        Project p = new Project();
+        p.setNo(rs.getInt("no"));
+        p.setTitle(rs.getString("tilte"));
+      }
+    }
+
+    // 2) 프로젝트 목록을 출력한다.
+    System.out.println("프로젝트들:");
+    if (projects.size() == 0) {
+      System.out.println("현재 등록된 프로젝트가 없습니다!");
+      return;
+    }
+
+    // 3) 작업을 등록할 프로젝트를 선택한다.
+    int selectedProjectNo = 0;
+    loop: while (true) {
+      String input = Prompt.inputString("프로젝트 번호?(취소: 빈 문자열) ");
+      if (input.length() == 0) {
+        System.out.println("작업 등록을 취소합니다.");
+        return;
+      }
+      selectedProjectNo = Integer.parseInt(input);
+      for (Project p : projects) {
+        if (p.getNo() == selectedProjectNo) {
+          break loop;
+        }
+      }
+      System.out.println("유효하지 않은 프로젝트 번호 입니다.");
+    }
+
+    // 4) 작업 정보를 입력 받는다.
     Task t = new Task();
     t.setContent(Prompt.inputString("내용? "));
     t.setDeadline(Prompt.inputDate("마감일? "));
