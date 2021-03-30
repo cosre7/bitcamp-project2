16f5d2a58fc1c5ce45ec2b6a391b9823bb039e2e
cosre7 <cosre7@gmail.com>
2021-03-30 11:11:46 +0900

    dao step2

--- bitcamp-project-client/app/src/main/java/com/eomcs/pms/dao/BoardDao.java
+++ bitcamp-project-client/app/src/main/java/com/eomcs/pms/dao/BoardDao.java
@@ -3,11 +3,15 @@ package com.eomcs.pms.dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 import com.eomcs.pms.domain.Board;
+import com.eomcs.pms.domain.Member;
 
 public class BoardDao {
   public static int insert(Board board) throws Exception {
-    try (Connection con = DriverManager.getConnection( 
+    try (Connection con = DriverManager.getConnection( // try : 단지 자동 close를 위해 사용, 예외용 아님
         "jdbc:mysql://localhost:3306/studydb?user=study&password=1111");
         PreparedStatement stmt =
             con.prepareStatement("insert into pms_board(title, content, writer) values(?,?,?)");) {
@@ -18,5 +22,41 @@ public class BoardDao {
 
       return stmt.executeUpdate();
     }
+  }
+
+  public static List<Board> findAll() throws Exception {
+    ArrayList<Board> list = new ArrayList<>();
+    try (Connection con = DriverManager.getConnection( 
+        "jdbc:mysql://localhost:3306/studydb?user=study&password=1111");
+        PreparedStatement stmt = con.prepareStatement( 
+            "select"
+                + " b.no,"
+                + " b.title,"
+                + " b.cdt,"
+                + " b.vw_cnt,"
+                + " b.like_cnt,"
+                + " m.no as writer_no,"
+                + " m.name as writer_name"
+                + " from pms_board b"
+                + "    inner join pms_member m on m.no=b.writer"
+                + " order by b.no desc");
+        ResultSet rs = stmt.executeQuery()) {
+
+      while (rs.next()) {
+        Board board = new Board();
+        board.setNo(rs.getInt("no"));
+        board.setTitle(rs.getString("title"));
+        board.setRegisteredDate(rs.getDate("cdt"));
+        board.setViewCount(rs.getInt("vw_cnt"));
+
+        Member writer = new Member();
+        writer.setNo(rs.getInt("writer_no"));
+        writer.setName(rs.getString("writer_name"));
+        board.setWriter(writer);
+
+        list.add(board);
+      }
+    }
+    return list;
   }
 }
--- bitcamp-project-client/app/src/main/java/com/eomcs/pms/handler/BoardListHandler.java
+++ bitcamp-project-client/app/src/main/java/com/eomcs/pms/handler/BoardListHandler.java
@@ -1,9 +1,8 @@
 package com.eomcs.pms.handler;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.util.List;
+import com.eomcs.pms.dao.BoardDao;
+import com.eomcs.pms.domain.Board;
 
 public class BoardListHandler implements Command {
 
@@ -11,30 +10,14 @@ public class BoardListHandler implements Command {
   public void service() throws Exception {
     System.out.println("[게시글 목록]");
 
-    try (Connection con = DriverManager.getConnection( 
-        "jdbc:mysql://localhost:3306/studydb?user=study&password=1111");
-        PreparedStatement stmt = con.prepareStatement( 
-            "select"
-                + " b.no,"
-                + " b.title,"
-                + " b.cdt,"
-                + " b.vw_cnt,"
-                + " b.like_cnt,"
-                + " m.no as writer_no,"
-                + " m.name as writer_name"
-                + " from pms_board b"
-                + "    inner hoin pms_member m on m.no=b.writer"
-                + " order by b.no desc");
-        ResultSet rs = stmt.executeQuery()) {
-
-      while (rs.next()) {
-        System.out.printf("%d, %s, %s, %s, %d\n", 
-            rs.getInt("no"), 
-            rs.getString("title"), 
-            rs.getString("writer_name"), 
-            rs.getDate("cdt"), // getString을 하게되면 시간까지!
-            rs.getInt("vw_cnt"));
-      }
+    List<Board> boards = BoardDao.findAll();
+    for (Board b : boards) {
+      System.out.printf("%d, %s, %s, %s, %d\n", 
+          b.getNo(), 
+          b.getTitle(), 
+          b.getWriter().getName(), 
+          b.getRegisteredDate(), // getString을 하게되면 시간까지!
+          b.getViewCount());
     }
   }
 }
