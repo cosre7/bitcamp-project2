7bef9cf8eeb74274ded90eb6c360f1ab0ca5ddd8
cosre7 <cosre7@gmail.com>
2021-03-30 15:41:22 +0900

    31-a

--- bitcamp-project-client/app/src/main/java/com/eomcs/pms/handler/BoardUpdateHandler.java
+++ bitcamp-project-client/app/src/main/java/com/eomcs/pms/handler/BoardUpdateHandler.java
@@ -6,13 +6,21 @@ import com.eomcs.util.Prompt;
 
 public class BoardUpdateHandler implements Command {
 
+  // 핸들러가 사용할 DAO : 의존 객체(dependency)
+  BoardDao boardDao;
+
+  // DAO 객체는 이 클래스가 작업하는데 필수 객체이기 때문에
+  // 생성자를 통해 반드시 주입 받도록 한다.
+  public BoardUpdateHandler(BoardDao boardDao) {
+    this.boardDao = boardDao;
+  }
+
   @Override
   public void service() throws Exception {
     System.out.println("[게시글 변경]");
 
     int no = Prompt.inputInt("번호? ");
 
-    BoardDao boardDao = new BoardDao();
     Board board = boardDao.findByNo(no);
     if (board == null) {
       System.out.println("해당 번호의 게시글이 없습니다.");
