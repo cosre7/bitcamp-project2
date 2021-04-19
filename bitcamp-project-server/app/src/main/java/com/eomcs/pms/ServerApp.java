c6181f29b06aeac603df5c7d23334c85be038362
cosre7 <cosre7@gmail.com>
2021-04-19 15:25:36 +0900

    27-d-9

--- bitcamp-project-server/app/src/main/java/com/eomcs/pms/ServerApp.java
+++ bitcamp-project-server/app/src/main/java/com/eomcs/pms/ServerApp.java
@@ -1,13 +1,41 @@
 package com.eomcs.pms;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Parameter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import com.eomcs.mybatis.MybatisDaoFactory;
+import com.eomcs.pms.dao.BoardDao;
+import com.eomcs.pms.dao.MemberDao;
+import com.eomcs.pms.dao.ProjectDao;
+import com.eomcs.pms.dao.TaskDao;
+import com.eomcs.pms.handler.Command;
+import com.eomcs.pms.handler.MemberValidator;
+import com.eomcs.pms.service.BoardService;
+import com.eomcs.pms.service.MemberService;
+import com.eomcs.pms.service.ProjectService;
+import com.eomcs.pms.service.TaskService;
+import com.eomcs.pms.service.impl.DefaultBoardService;
+import com.eomcs.pms.service.impl.DefaultMemberService;
+import com.eomcs.pms.service.impl.DefaultProjectService;
+import com.eomcs.pms.service.impl.DefaultTaskService;
+import com.eomcs.stereotype.Component;
 
 public class ServerApp {
 
@@ -16,19 +44,70 @@ public class ServerApp {
   // 서버의 상태를 설정
   boolean isStop;
 
+  //객체를 보관할 컨테이너 준비
+  Map<String,Object> objMap = new HashMap<>();
+
   public static void main(String[] args) {
-    ServerApp app = new ServerApp(8888);
-    app.service();
+
+    try {
+      ServerApp app = new ServerApp(8888);
+      app.service();
+
+    } catch (Exception e) {
+      System.out.println("서버를 시작 하는 중에 오류 발생!");
+      e.printStackTrace();
+    }
   }
 
   public ServerApp(int port) {
     this.port = port;
   }
 
-  public void service() { // service -> main 스레드가 호출하는 메서드
+  public void service() throws Exception { // service -> main 스레드가 호출하는 메서드
 
     // 스레드풀 준비
     ExecutorService threadPool = Executors.newCachedThreadPool();
+
+    // 1) Mybatis 프레임워크 관련 객체 준비
+    // => Mybatis 설정 파일을 읽을 입력 스트림 객체 준비
+    InputStream mybatisConfigStream = Resources.getResourceAsStream(
+        "com/eomcs/pms/conf/mybatis-config.xml");
+
+    // => SqlSessionFactory 객체 준비
+    SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(mybatisConfigStream);
+
+    // => DAO가 사용할 SqlSession 객체 준비
+    //    - 수동 commit 으로 동작하는 SqlSession 객체를 준비한다.
+    SqlSession sqlSession = sqlSessionFactory.openSession(false);// Mybatis 설정 파일을 읽을 입력 스트림 객체 준비
+
+    // 2) DAO 구현체를 자동으로 만들어주는 공장 객체를 준비한다.
+    MybatisDaoFactory daoFactory = new MybatisDaoFactory(sqlSession);
+
+    // 3) 서비스 객체가 사용할 DAO 객체 준비
+    BoardDao boardDao = daoFactory.createDao(BoardDao.class);
+    MemberDao memberDao = daoFactory.createDao(MemberDao.class);
+    ProjectDao projectDao = daoFactory.createDao(ProjectDao.class);
+    TaskDao taskDao = daoFactory.createDao(TaskDao.class);
+
+    // 4) Command 구현체가 사용할 의존 객체(서비스 객체 + 도우미 객체) 준비
+    // => 서비스 객체 생성
+    BoardService boardService = new DefaultBoardService(sqlSession, boardDao);
+    MemberService memberService = new DefaultMemberService(sqlSession, memberDao);
+    ProjectService projectService = new DefaultProjectService(sqlSession, projectDao, taskDao);
+    TaskService taskService = new DefaultTaskService(sqlSession, taskDao);
+
+    // => 도우미 객체 생성
+    MemberValidator memberValidator = new MemberValidator(memberService);
+
+    // => Command 구현체가 사용할 의존 객체를 보관
+    objMap.put("boardService", boardService);
+    objMap.put("memberService", memberService);
+    objMap.put("projectService", projectService);
+    objMap.put("taskService", taskService);
+    objMap.put("memberValidator", memberValidator);
+
+    // 5) Command 구현체를 자동 생성하여 맵에 등록
+    registerCommands();
 
     // 클라이언트 연결을 기다리는 서버 소켓 생성
     try (ServerSocket serverSocket = new ServerSocket(this.port)) {
@@ -153,5 +232,122 @@ public class ServerApp {
       // 하지만 메인 스레드가 종료되고 다른 스레들이 남아있기 때문에 서버가 종료되고 난 후에도
       // Console 자체는 종료되지 않고 남아있다 -> 강제종료해야한다.
     } catch (Exception e) {}
+  }
+
+  private void registerCommands() throws Exception {
+
+    // 패키지에 소속된 모든 클래스의 타입 정보를 알아낸다.
+    ArrayList<Class<?>> components = new ArrayList<>();
+    loadComponents("com.eomcs.pms.handler", components);
+
+    for (Class<?> clazz : components) {
+
+      // 클래스 목록에서 클래스 정보를 한 개 꺼내, Command 구현체인지 검사한다.
+      if (!isCommand(clazz)) {
+        continue;
+      }
+
+      // 클래스 정보를 이용하여 객체를 생성한다.
+      Object command = createCommand(clazz);
+
+      // 클래스 정보에서 @Component 애노테이션 정보를 가져온다.
+      Component compAnno = clazz.getAnnotation(Component.class);
+
+      // 애노테이션 정보에서 맵에 객체를 저장할 때 키로 사용할 문자열 꺼낸다.
+      String key = null;
+      if (compAnno.value().length() == 0){
+        key = clazz.getName(); // 키로 사용할 문자열이 없으면 클래스 이름을 키로 사용한다.
+      } else {
+        key = compAnno.value();
+      }
+
+      // 생성된 객체를 객체 맵에 보관한다.
+      objMap.put(key, command);
+
+      System.out.println("인스턴스 생성 ===> " + command.getClass().getName());
+    }
+  }
+
+  private boolean isCommand(Class<?> type) {
+    // 클래스가 아니라 인터페이스라면 무시한다.
+    if (type.isInterface()) {
+      return false;
+    }
+
+    // 클래스의 인터페이스 목록을 꺼낸다.
+    Class<?>[] interfaces = type.getInterfaces();
+
+    // 클래스가 구현한 인터페이스 중에서 Command 인터페이스가 있는지 조사한다.
+    for (Class<?> i : interfaces) {
+      if (i == Command.class) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  private void loadComponents(String packageName, ArrayList<Class<?>> components) throws Exception {
+
+    // 패키지의 '파일 시스템 경로'를 알아낸다.
+    File dir = Resources.getResourceAsFile(packageName.replaceAll("\\.", "/"));
+
+    if (!dir.isDirectory()) {
+      throw new Exception("유효한 패키지가 아닙니다.");
+    }
+
+    File[] files = dir.listFiles(f -> {
+      if (f.isDirectory() || f.getName().endsWith(".class"))
+        return true;
+      return false;
+    });
+
+    for (File file : files) {
+      if (file.isDirectory()) {
+        loadComponents(packageName + "." + file.getName(), components);
+      } else {
+        String className = packageName + "." + file.getName().replace(".class", "");
+        try {
+          Class<?> clazz = Class.forName(className);
+          if (clazz.getAnnotation(Component.class) != null) {
+            components.add(clazz);
+          }
+        } catch (Exception e) {
+          System.out.println("클래스 로딩 오류: " + className);
+        }
+      }
+    }
+  }
+
+  private Object createCommand(Class<?> clazz) throws Exception {
+    // 생성자 정보를 알아낸다. 첫 번째 생성자만 꺼낸다.
+    Constructor<?> constructor = clazz.getConstructors()[0];
+
+    // 생성자의 파라미터 정보를 알아낸다.
+    Parameter[] params = constructor.getParameters();
+
+    // 생성자를 호출할 때 넘겨 줄 값을 담을 컬렉션을 준비한다.
+    ArrayList<Object> args = new ArrayList<>();
+
+    // 각 파라미터의 타입을 알아낸 후 objMap에서 찾는다.
+    for (Parameter p : params) {
+      Class<?> paramType = p.getType();
+      args.add(findDependency(paramType));
+    }
+
+    // 생성자를 호출하여 인스턴스를 생성한다.
+    return constructor.newInstance(args.toArray());
+  }
+
+  private Object findDependency(Class<?> type) {
+    // 맵에서 값 목록을 꺼낸다.
+    Collection<?> values = objMap.values();
+
+    for (Object obj : values) {
+      if (type.isInstance(obj)) {
+        return obj;
+      }
+    }
+    return null;
   }
 }
