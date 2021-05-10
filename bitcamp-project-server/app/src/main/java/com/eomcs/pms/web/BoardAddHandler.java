5d9d2c5f07ca29365e9b31c2ab83a7eb81547852
cosre7 <cosre7@gmail.com>
2021-05-10 10:03:39 +0900

    31-a board

--- bitcamp-project-server/app/src/main/java/com/eomcs/pms/web/BoardAddHandler.java
+++ bitcamp-project-server/app/src/main/java/com/eomcs/pms/web/BoardAddHandler.java
@@ -3,53 +3,74 @@ package com.eomcs.pms.web;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import javax.servlet.GenericServlet;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import com.eomcs.pms.domain.Board;
 import com.eomcs.pms.domain.Member;
 import com.eomcs.pms.service.BoardService;
 
 @SuppressWarnings("serial")
 @WebServlet("/board/add")
-public class BoardAddHandler extends GenericServlet {
+public class BoardAddHandler extends HttpServlet {
 
   @Override
-  public void service(ServletRequest request, ServletResponse response)
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+  // GET 요청에 대해서는 이 서블릿은 동작하지 않는다. post 요청에 대해서만 동작하게 된다.
       throws ServletException, IOException {
 
     BoardService boardService = (BoardService) request.getServletContext().getAttribute("boardService");
 
-    response.setContentType("text/plain;charset=UTF-8");
-    PrintWriter out = response.getWriter();
-
-    out.println("[게시글 등록]");
-
     Board b = new Board();
+
+    // 클라이언트가 POST 요청으로 보낸 데이터가 UTF-8임을 알려준다.
+    request.setCharacterEncoding("UTF-8");
 
     b.setTitle(request.getParameter("title"));
     b.setContent(request.getParameter("content"));
 
     // 작성자는 로그인 사용자이다.
-    HttpServletRequest httpRequest = (HttpServletRequest) request;
+    HttpServletRequest httpRequest = request;
     Member loginUser = (Member) httpRequest.getSession().getAttribute("loginUser"); // 클라이언트가 요청할 때 세션이 만들어진다.
     b.setWriter(loginUser);
 
+    response.setContentType("text/html;charset=UTF-8");
+    PrintWriter out = response.getWriter();
+
+    out.println("<!DOCTYPE html>");
+    out.println("<html>");
+    out.println("<head>");
+    out.println("<title>게시글 등록</title>");
+
+    out.println("[게시글 등록]");
+
     try {
       boardService.add(b);
-      out.println("게시글을 등록하였습니다.");
+
+      out.println("<meta http-equiv='Refresh' content='1;url=list'>");  
+      // '1;list': 1초 후에 다시 list를 요청하세요~ => 무조건 head 안에 들어있어야 하는 내용!
+      out.println("</head>");
+      out.println("<body>");
+      out.println("<h1>게시글 등록</h1>");
+      out.println("<p>게시글을 등록했습니다.</p>");
+
     } catch (Exception e) {
       // 상세 오류 내용을 StringWriter로 출력한다.
       StringWriter strWriter = new StringWriter();
       PrintWriter printWriter = new PrintWriter(strWriter);
       e.printStackTrace(printWriter);
 
-      // StringWriter에 들어 있는 출력 내용을 꺼내 클라이언트로 보낸다.
-      out.println(strWriter.toString());
+      out.println("</head>"); // add하다가 에러가 발생하면 바로 head 멈춤
+      out.println("<body>");
+      out.println("<h1>게시글 등록오류</h1>");
+      out.printf("<pre>%s</pre>\n", strWriter.toString());
+      out.println("<p><a href='list'>목록</a></p>"); // 목록으로 돌아갈 수 있는 링크
     }
+
+    out.println("</body>");
+    out.println("</html>");
   }
 }
 
--- bitcamp-project-server/app/src/main/java/com/eomcs/pms/web/BoardListHandler.java
+++ bitcamp-project-server/app/src/main/java/com/eomcs/pms/web/BoardListHandler.java
@@ -4,68 +4,86 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.List;
-import javax.servlet.Servlet;
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import com.eomcs.pms.domain.Board;
 import com.eomcs.pms.service.BoardService;
 
+@SuppressWarnings("serial")
 @WebServlet("/board/list")
-public class BoardListHandler implements Servlet {
+public class BoardListHandler extends HttpServlet {
 
   @Override
-  public void service(ServletRequest request, ServletResponse response)
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
     // 클라이언트가 /board/list를 요청하면 톰캣 서버가 이 메서드를 호출한다.
 
     BoardService boardService = (BoardService) request.getServletContext().getAttribute("boardService");
 
-    response.setContentType("text/plain;charset=UTF-8");
+    response.setContentType("text/html;charset=UTF-8"); // 브라우저야~ html 규칙에 따라서 만들어줘~
+    // 서블릿을 통해 실행할 경우 charset이 utf-8로 설정해주기 때문에 html 규칙 안에 meta charset=utf-8 을
+    // 따로 설정할 필요가 없다.
     PrintWriter out = response.getWriter();
 
-    out.println("[게시글 목록]");
+    out.println("<!DOCTYPE html>");
+    out.println("<html>");
+    out.println("<head>");
+    out.println("<title>게시글 목록</title>");
+    out.println("</head>");
+    out.println("<body>");
+    out.println("<h1>게시글 목록</h1>");
+
+    out.println("<p><a href='form.html'>새 글</a></p>");
 
     try {
       List<Board> boards = boardService.list();
 
+      out.println("<table border='1'>");
+      out.println("<thead>");
+      out.println("<tr>");
+      out.println("<th>번호</th> <th>제목</th> <th>작성자</th> <th>등록일</th> <th>조회수</th>");
+      out.println("</tr>");
+      out.println("</thead>");
+      out.println("<tbody>");
+      // println을 쓰는 이유 -> 페이지 소스보기를 할 때 한 줄씩 띄워서 보기 편하게 하기 위함일 뿐
+      // 엡 브라우저와는 관계가 없다.
+
       for (Board b : boards) {
-        out.printf("%d, %s, %s, %s, %d\n", 
+        out.printf("<tr>"
+            + " <td>%d</td>"
+            + " <td><a href='detail?no=%1$d'>%s</a></td>" // board/detail을 보고 싶을 때 -> list와 앞까지는 같은 경루 => 상대경로 detail만 적는다.
+            + " <td>%s</td>"
+            + " <td>%s</td>"
+            + " <td>%d</td> </tr>\n",  
+            // \n을 해주지 않으면 브라우저에서 소스를 볼 때 한 줄로 쭈우우욱 나온다.
             b.getNo(), 
             b.getTitle(), 
             b.getWriter().getName(),
             b.getRegisteredDate(),
             b.getViewCount());
       }
+      out.println("</tbody>");
+      out.println("</table>");
+
+      out.println("<form action='search' method='get'>");
+      out.println("<input type='text' name='keyword'>");
+      out.println("<button type='submit'>검색</button>"); //type='submit' 생략하면 기본이 submit
+      out.println("</form>");
+
     } catch (Exception e) {
       // 상세 오류 내용을 StringWriter로 출력한다.
       StringWriter strWriter = new StringWriter();
       PrintWriter printWriter = new PrintWriter(strWriter);
       e.printStackTrace(printWriter);
 
-      // StringWriter에 들어 있는 출력 내용을 꺼내 클라이언트로 보낸다.
-      out.println(strWriter.toString());
+      out.printf("<pre>%s</pre>\n", strWriter.toString());
     }
-  }
 
-  @Override
-  public void init(ServletConfig config) throws ServletException {
-  }
-
-  @Override
-  public void destroy() {
-  }
-
-  @Override
-  public String getServletInfo() {
-    return null;
-  }
-
-  @Override
-  public ServletConfig getServletConfig() {
-    return null;
+    out.println("</body>");
+    out.println("</html>");
   }
 }
 
