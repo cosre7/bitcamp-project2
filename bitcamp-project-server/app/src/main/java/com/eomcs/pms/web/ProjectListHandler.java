26c1a069bc534df1af95087c84593f94aa5176a5
cosre7 <cosre7@gmail.com>
2021-05-10 10:15:23 +0900

    31-a ProjectListHandler

--- bitcamp-project-server/app/src/main/java/com/eomcs/pms/web/ProjectListHandler.java
+++ bitcamp-project-server/app/src/main/java/com/eomcs/pms/web/ProjectListHandler.java
@@ -4,32 +4,48 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.List;
-import javax.servlet.Servlet;
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import com.eomcs.pms.domain.Member;
 import com.eomcs.pms.domain.Project;
 import com.eomcs.pms.service.ProjectService;
 
+@SuppressWarnings("serial")
 @WebServlet("/project/list")
-public class ProjectListHandler implements Servlet {
+public class ProjectListHandler extends HttpServlet {
 
   @Override
-  public void service(ServletRequest request, ServletResponse response)
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
 
     ProjectService projectService = (ProjectService) request.getServletContext().getAttribute("projectService");
-    // servletContext라는 보관소에 저장된 projectService라고 저장된 객체를 리턴해줘~
 
-    response.setContentType("text/plain;charset=UTF-8");
+    response.setContentType("text/html;charset=UTF-8"); 
     PrintWriter out = response.getWriter();
-    out.println("[프로젝트 목록]");
+
+    out.println("<!DOCTYPE html>");
+    out.println("<html>");
+    out.println("<head>");
+    out.println("<title>프로젝트</title>");
+    out.println("</head>");
+    out.println("<body>");
+    out.println("<h1>프로젝트</h1>");
+
+    out.println("<p><a href='form.html'>새 프로젝트</a></p>");
 
     try {
       List<Project> projects = projectService.list();
+
+      out.println("<table border='1'>");
+      out.println("<thead>");
+      out.println("<tr>");
+      out.println("<th>번호</th> <th>프로젝트명</th> <th>기간</th> <th>팀장</th> <th>팀원</th>");
+      out.println("</tr>");
+      out.println("</thead>");
+      out.println("<tbody>");
 
       for (Project p : projects) {
 
@@ -38,13 +54,18 @@ public class ProjectListHandler implements Servlet {
         List<Member> members = p.getMembers();
         for (Member m : members) {
           if (strBuilder.length() > 0) {
-            strBuilder.append("/");
+            strBuilder.append(",");
           }
           strBuilder.append(m.getName());
         }
 
         // 2) 프로젝트 정보를 출력
-        out.printf("%d, %s, %s, %s, %s, [%s]\n", 
+        out.printf("<tr>"
+            + " <td>%d</td>"
+            + " <td><a href='detail?no=%1$d'>%s</a></td>" // board/detail을 보고 싶을 때 -> list와 앞까지는 같은 경루 => 상대경로 detail만 적는다.
+            + " <td>%s ~ %s</td>"
+            + " <td>%s</td>"
+            + " <td>%s</td> </tr>\n",  
             p.getNo(), 
             p.getTitle(), 
             p.getStartDate(),
@@ -52,6 +73,13 @@ public class ProjectListHandler implements Servlet {
             p.getOwner().getName(),
             strBuilder.toString());
       }
+      out.println("</tbody>");
+      out.println("</table>");
+
+      out.println("<form action='search' method='get'>");
+      out.println("<input type='text' name='keyword'>");
+      out.println("<button type='submit'>검색</button>"); //type='submit' 생략하면 기본이 submit
+      out.println("</form>");
 
     } catch (Exception e) {
       // 상세 오류 내용을 StringWriter로 출력한다.
@@ -59,29 +87,11 @@ public class ProjectListHandler implements Servlet {
       PrintWriter printWriter = new PrintWriter(strWriter);
       e.printStackTrace(printWriter);
 
-      // StringWriter에 들어 있는 출력 내용을 꺼내 클라이언트로 보낸다.
-      out.println(strWriter.toString());
+      out.printf("<pre>%s</pre>\n", strWriter.toString());
     }
-  }
 
-  @Override
-  public void init(ServletConfig config) throws ServletException {
-
-  }
-
-  @Override
-  public void destroy() {
-
-  }
-
-  @Override
-  public String getServletInfo() {
-    return null;
-  }
-
-  @Override
-  public ServletConfig getServletConfig() {
-    return null;
+    out.println("</body>");
+    out.println("</html>");
   }
 }
 
