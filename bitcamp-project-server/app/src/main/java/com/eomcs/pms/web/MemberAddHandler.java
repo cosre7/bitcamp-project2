f2b8ab9372d21fe765654e540e2606b6f6398196
cosre7 <cosre7@gmail.com>
2021-05-12 16:36:31 +0900

    31-c-Refresh->Member 적용

--- bitcamp-project-server/app/src/main/java/com/eomcs/pms/web/MemberAddHandler.java
+++ bitcamp-project-server/app/src/main/java/com/eomcs/pms/web/MemberAddHandler.java
@@ -43,6 +43,9 @@ public class MemberAddHandler extends HttpServlet {
     out.println("<html>");
     out.println("<head>");
     out.println("<title>회원 등록</title>");
+    out.println("</head>");
+    out.println("<body>");
+    out.println("<h1>회원 등록</h1>");
 
     try {
       Member m = new Member();
@@ -84,22 +87,31 @@ public class MemberAddHandler extends HttpServlet {
       }
       memberService.add(m);
 
-      out.println("<meta http-equiv='Refresh' content='1;url=list'>");
-      out.println("</head>");
-      out.println("<body>");
-      out.println("<h1>회원 등록</h1>");
       out.println("<p>회원을 등록했습니다.</p>");
+
+      // 응답헤더에 리프레시 정보를 설정한다.
+      response.setHeader("Refresh", "1;url=list"); // 1초 후에 url=list로 가라!
+
+      // 질문!
+      //   클라이언트에게 응답할 때 헤더를 먼저 보내고 콘텐트를 나중에 보내는데 
+      //   위의 코드를 보면 pringln()을 이용하여 콘텐트를 먼저 출력한 다음에
+      //   응답 헤더를 설정하는데 이것이 가능한가요?
+      // - println()을 실행할 때 출력 내용은 모두 버퍼로 보낸다.
+      // - 즉 아직 클라이언트에게 응답한 상태가 아니기 때문에 응답 헤더를 설정할 수 있는 것이다.
+
+      // - 출력 버퍼를 다 채워야 보내지는데 위의 내용으로는 보내지지 않기 때문
+      // - 어차피 버퍼에 쌓이는 중이지 보내진 상태가 아니기 때문에 서버로 보내지지 않는다
+      //   때문에 리프레시 정보를 후에 설정해도 상관이 없다.
+      // - 버퍼에 쌓인 내용은 메서드 호출이 모두 끝난 후에 서버로 보내진다.
+      // - 정보 설정 전에 이미 버퍼가 쌓여버리면 서버로 보내져버려서 리프레시가 되지 않는다.
 
     } catch (Exception e) {
       StringWriter strWriter = new StringWriter();
       PrintWriter printWriter = new PrintWriter(strWriter);
       e.printStackTrace(printWriter);
 
-      out.println("</head>");
-      out.println("<body>");
-      out.println("<h1>회원 등록 오류</h1>");
-      out.printf("<pre>%s</pre>\n", strWriter.toString());
-      out.println("<p><a href='list'>목록</a></p>");
+      out.printf("<pre>%s</pre>\n", strWriter.toString()); 
+      out.println("<p><a href='list'>목록</a></p>"); // 오류가 발생했을 때에는 리프레시가 자동으로 안된다.
     }
 
     out.println("</body>");
