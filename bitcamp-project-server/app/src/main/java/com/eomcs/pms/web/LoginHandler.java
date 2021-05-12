b244fb3b6cba5645953320d3ef5f0c2c360a44a9
cosre7 <cosre7@gmail.com>
2021-05-12 16:55:26 +0900

    31-c-redirect login/logout

--- bitcamp-project-server/app/src/main/java/com/eomcs/pms/web/LoginHandler.java
+++ bitcamp-project-server/app/src/main/java/com/eomcs/pms/web/LoginHandler.java
@@ -72,21 +72,32 @@ public class LoginHandler extends HttpServlet {
       if (member == null) {
         // 로그인 실패한다면 세션 객체의 모든 내용을 삭제한다.
         request.getSession().invalidate(); 
-        out.println("<meta http-equiv='Refresh' content='1;url=login'>");
         out.println("</head>");
         out.println("<body>");
         out.println("<h1>로그인 결과</h1>");
         out.println("<p>사용자 정보가 맞지 않습니다.</p>");
 
+        response.sendRedirect("login"); // 로그인으로 가랏 -> 상대경로
+
+        // 주석 가져오기
+
+        // 로그인을 요청한 후 응답 결과를 보면 위와 같다.
+        // 여기서 주목해야 할 사항은 응답에 println()으로 출력한 결과가 없다는 것이다.
+        // 왜?
+        // - sendRedirect()를 호출하면 버퍼에 들어 있는 출력 내용을 버린다.
+        // - 응답할 때 콘텐트를 보내지 않기 때문이다.
+        // - 따라서 리다이렉트를 할 생각이라면 콘텐트를 출력하지 말라!! -> 부질없는 짓이다. 어차피 무시됨
+        // - 302 응답헤더만 보낼 뿐
       } else {
         // 로그인 성공한다면, 로그인 사용자 정보를 세션 객체에 보관한다.
         request.getSession().setAttribute("loginUser", member);      
 
-        out.println("<meta http-equiv='Refresh' content='1;url=userInfo'>");
         out.println("</head>");
         out.println("<body>");
         out.println("<h1>로그인 결과</h1>");
         out.printf("<p>%s 님 환영합니다.</p>\n", member.getName());
+
+        response.sendRedirect("userInfo"); // userInfo로 가랏 -> 상대경로
       }
 
 
