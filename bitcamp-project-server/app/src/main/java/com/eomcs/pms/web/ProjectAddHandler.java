c4467dfd0050a0223ccd1a495848c2c164d1370c
cosre7 <cosre7@gmail.com>
2021-05-13 18:42:27 +0900

    31-e-세션

--- bitcamp-project-server/app/src/main/java/com/eomcs/pms/web/ProjectAddHandler.java
+++ bitcamp-project-server/app/src/main/java/com/eomcs/pms/web/ProjectAddHandler.java
@@ -1,7 +1,6 @@
 package com.eomcs.pms.web;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.Date;
 import java.util.ArrayList;
 import javax.servlet.ServletException;
@@ -9,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import com.eomcs.pms.domain.Member;
 import com.eomcs.pms.domain.Project;
 import com.eomcs.pms.service.ProjectService;
@@ -18,58 +18,28 @@ import com.eomcs.pms.service.ProjectService;
 public class ProjectAddHandler extends HttpServlet {
 
   @Override
-  protected void doGet(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
-
-    response.setContentType("text/html;charset=UTF-8");
-    PrintWriter out = response.getWriter();
-
-    out.println("<!DOCTYPE html>");
-    out.println("<html>");
-    out.println("<head>");
-    out.println("<meta charset='UTF-8'>");
-    out.println("<title>새 프로젝트</title>");
-    out.println("</head>");
-    out.println("<body>");
-    out.println("<h1>새 프로젝트</h1>");
-    out.println("<form action='add' method='post'>");
-    out.println("제목: <input type='text' name='title'><br>");
-    out.println("내용: <textarea name='content' rows='10' cols='60'></textarea><br>");
-    out.println("시작일: <input type='date' name='startDate'><br>");
-    out.println("종료일: <input type='date' name='endDate'><br>");
-    out.println("팀원: <br>");
-
-    // 팀원 출력은 다른 서블릿에게 맡긴다.
-    request.getRequestDispatcher("/project/member/list").include(request, response);
-
-    // 인클루딩은 이전에 출력한 내용을 그대로 유지한다.
-    // 인클루딩 서블릿을 실행한 후 리턴된 뒤에 수행하는 출력도 유효하다.
-    // 
-
-    out.println("<p><input type='submit' value='등록'>");
-    out.println("<a href='list'>목록</a></p>");
-
-    out.println("</form>");
-    out.println("</body>");
-    out.println("</html>");
-  }
-
-  @Override
   protected void doPost(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
 
     ProjectService projectService = (ProjectService) request.getServletContext().getAttribute("projectService");
 
+    HttpSession session = request.getSession();
+
     try {
       Project p = new Project();
-      p.setTitle(request.getParameter("title"));
-      p.setContent(request.getParameter("content"));
-      p.setStartDate(Date.valueOf(request.getParameter("startDate")));
-      p.setEndDate(Date.valueOf(request.getParameter("endDate")));
+      // 프로젝트 등록 1단계에서 입력한 내용이 세션에 보관되어 있다.
+      p.setTitle((String) session.getAttribute("title"));
 
-      Member loginUser = (Member) request.getSession().getAttribute("loginUser");
+      // 프로젝트 등록 2단계에서 입력한 내용이 세션에 보관되어 있다.
+      p.setContent((String) session.getAttribute("content"));
+      p.setStartDate(Date.valueOf((String) session.getAttribute("startDate")));
+      p.setEndDate(Date.valueOf((String) session.getAttribute("endDate")));
+
+      // 사용자 로그인 정보는 세션에 보관되어 있다.
+      Member loginUser = (Member) session.getAttribute("loginUser");
       p.setOwner(loginUser);
 
+      // 프로젝트 팀원 정보는 파라미터에 있다.
       // ...&member=1&member=18&member=23
       String[] values = request.getParameterValues("member");
       ArrayList<Member> memberList = new ArrayList<>();
