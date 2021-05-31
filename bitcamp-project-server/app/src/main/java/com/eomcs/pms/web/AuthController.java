bc7455c06ed627caffdc2a2a23f2a4dd2089003e
cosre7 <cosre7@gmail.com>
2021-05-31 15:02:25 +0900

    AuthController 처리

--- bitcamp-project-server/app/src/main/java/com/eomcs/pms/web/AuthController.java
+++ bitcamp-project-server/app/src/main/java/com/eomcs/pms/web/AuthController.java
@@ -1,8 +1,8 @@
 package com.eomcs.pms.web;
 
 import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,48 +20,45 @@ public class AuthController {
   }
 
   @GetMapping("/login")
-  public String form(HttpServletRequest request, HttpServletResponse response) throws Exception {
+  public String form() throws Exception {
     return "/jsp/login_form.jsp";
   } 
 
   @PostMapping("/login")
-  public String login(HttpServletRequest request, HttpServletResponse response) throws Exception {
+  public String login(
+      String email, String password, String saveEmail, 
+      HttpSession session, HttpServletResponse response) throws Exception {
 
-    String email = request.getParameter("email");
-    String password = request.getParameter("password");
-
-    // 클라이언트에게 쿠키를 보내기
-    if (request.getParameter("saveEmail") != null) {
+    if (saveEmail != null) {
       Cookie cookie = new Cookie("email", email);
       cookie.setMaxAge(60 * 60 * 24 * 5); // 유효기간을 설정하지 않으면 웹브라우저가 실행되는 동안만 유지하라는 의미가 된다.
       response.addCookie(cookie);
     } else {
-      // 기존에 있는 쿠키도 제거한다.
       Cookie cookie = new Cookie("email", "");
-      cookie.setMaxAge(0);  // 유효기간(초)을 0으로 하면 웹브라우저는 email 이름으로 저장된 쿠키를 제거한다.
+      cookie.setMaxAge(0);  
       response.addCookie(cookie);
     }
 
     Member member = memberService.get(email, password);
 
     if (member == null) {
-      request.getSession().invalidate(); 
+      session.invalidate(); 
       return "/jsp/login_fail.jsp";
 
     } else {
-      request.getSession().setAttribute("loginUser", member);
+      session.setAttribute("loginUser", member);
       return "/jsp/login_success.jsp";
     }
   }
 
   @RequestMapping("/logout")
-  public String logout(HttpServletRequest request, HttpServletResponse response) throws Exception {
-    request.getSession().invalidate();
+  public String logout(HttpSession session) throws Exception {
+    session.invalidate();
     return "redirect:login";
   }
 
   @RequestMapping("/userInfo")
-  public String userInfo(HttpServletRequest request, HttpServletResponse response) throws Exception {
+  public String userInfo() throws Exception {
     return "/jsp/user_info.jsp";
   }
 }
